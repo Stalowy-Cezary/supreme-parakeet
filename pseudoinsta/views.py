9a04ade5ebeaef96253fc3df7030013eb878c4ed
Michal Beska <michal.beska@euvic.pl>
2023-01-28 13:46:47 +0100

    add image

--- pseudoinsta/views.py
+++ pseudoinsta/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Post
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 class PostListView(ListView):
 
     model = Post
     paginate_by = 50
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    template_name = "pseudoinsta/add_image.html"
+    model = Post
